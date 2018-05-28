259f3c79e8a42486db3855e4633cc32165609409
Marco Chávez <marcochavezf@hotmail.com>
2018-05-28 10:14:17 -0500

    remove other unused components

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,8 +1,6 @@
 import Auth from './auth/reducer';
 import App from './app/reducer';
-import Box from './box/reducer';
 import Todos from './todos/reducer';
-import DynamicChartComponent from './dynamicEchart/reducer';
 import ThemeSwitcher from './themeSwitcher/reducer';
 import LanguageSwitcher from './languageSwitcher/reducer';
 import DevReducers from '../customApp/redux/reducers';
@@ -12,8 +10,6 @@ export default {
   App,
   ThemeSwitcher,
   LanguageSwitcher,
-  Box,
   Todos,
-  DynamicChartComponent,
   ...DevReducers
 };
